91971ab089bea150cee7c2a72bf45c0ca6ee8f51
Andrew Ge-Hall <ge-hall@outlook.com>
2023-01-09 16:21:08 +1000

    fix: index error

--- lua/whid.lua
+++ lua/whid.lua
@@ -4,8 +4,9 @@ local api = vim.api
 local buf, win
 
 local function open_window()
+	print("open_window")
 	buf = api.nvim_create_buf(false, true)
-	
+
 	api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
 	-- get dimensions
