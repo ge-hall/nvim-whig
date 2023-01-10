3ba5ff6440efa2dda7d43c83442f3153e835ef99
Andrew Ge-Hall <ge-hall@outlook.com>
2023-01-10 10:15:18 +1000

    add headings

--- lua/whid.lua
+++ lua/whid.lua
@@ -54,16 +54,50 @@ local function open_window()
 	api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 	
 	-- create window
-	win = api.nvim_open_win(buf, true, opts)
 	local border_win = api.nvim_open_win(border_buf, true, border_opts)
+	win = api.nvim_open_win(buf, true, opts)
 	api.nvim_command("autocmd BufWipeout <buffer> exe 'silent bwipeout! '" .. border_buf)
 
-
 end
+
+local function center(text)
+	local width = api.nvim_win_get_width(0)
+	local shift = math.floor(width/2) - math.floor(string.len(text)/2)
+	return string.rep(" ", shift) .. text
+end
+
+local position = 0
+
+local function update_view(direction)
+	position = position + direction
+	if position < 0 then
+		position = 0
+	end
+
+	local result = vim.fn.systemlist("git diff-tree --no-commit-id --name-only -r HEAD~" .. position)
+
+	for k, v in pairs(result) do
+		result[k] = ' '..result[k]
+	end
+
+	api.nvim_buf_set_lines(buf, 0, -1, false, {
+		center('What have I done?'),
+		center("HEAD~" .. position),
+		''
+	})
+
+	api.nvim_buf_set_lines(buf, 3, -1, false, result)
+
+	api.nvim_buf_add_highlight(buf, -1, "WhidHeader", 0, 0, -1)
+	api.nvim_buf_add_highlight(buf, -1, "WhidSubHeader", 1, 0, -1)
+end
+
 
 local function whid()
 	print("whid")
 	open_window()
+	update_view()
+	api.nvim_win_set_cursor(win, {4, 0})
 end
 
 return {
